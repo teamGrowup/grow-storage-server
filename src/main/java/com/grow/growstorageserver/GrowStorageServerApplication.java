89df6fd0b32219193b6407e48542ef6019dee3dc
정태승 <hi563@g.skku.edu>
2024-11-18 21:41:18 +0900

    first commit

---
+++ src/main/java/com/grow/growstorageserver/GrowStorageServerApplication.java
@@ -0,0 +1,13 @@
+package com.grow.growstorageserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowStorageServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrowStorageServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grow/growstorageserver/GrowStorageServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grow.growstorageserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowStorageServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
